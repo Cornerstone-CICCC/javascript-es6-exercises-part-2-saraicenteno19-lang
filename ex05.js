8e7b24a7b61085cc8f4d18a8bc9c8f8df47f2947
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-05-28 02:54:07 -0400

    Exercise 05 completed

--- ex05.js
+++ ex05.js
@@ -1,3 +1,9 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
 
+const numbers = [1,3,7,9,10];
+const hasEven = numbers.some(function (num) {
+ return num % 2 === 0;
+});
+
+console.log(hasEven);
