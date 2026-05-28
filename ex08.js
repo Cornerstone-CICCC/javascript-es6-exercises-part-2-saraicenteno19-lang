c60091fc29b9f90ecdbb0241677f4b0b193b9556
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-05-28 02:59:29 -0400

    Exercise 08 completed

--- ex08.js
+++ ex08.js
@@ -1,3 +1,8 @@
 // Join Exercise
 // Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
 
+const words = ["I", "like", "learning", "programming", "with", "teacher", "Elmer"];
+
+const sentence = words.join(" ");
+
+console.log(sentence);
